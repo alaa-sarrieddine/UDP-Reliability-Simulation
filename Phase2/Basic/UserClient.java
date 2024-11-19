587b72e287bfe07ef6c8569cb4e30aef7af74f74
browhys <alaa.sra.as@gmail.com>
2024-11-19 17:44:25 +0200

    Implemented rough version of UserClient.java. (Needs testing)

--- Phase2/Basic/UserClient.java
+++ Phase2/Basic/UserClient.java
@@ -1,5 +1,27 @@
 package Basic;
-
+import java.net.*;
 public class UserClient {
-    
+    public static void main(String[] args)throws Exception {
+        //We make sure sure inputs the source name and the destination.
+        if (args.length < 2) {
+            System.out.println("Please enter source, and destination(java Userclient <source> <destination>)");
+            return;
+        }
+        DatagramSocket client = new DatagramSocket();
+        InetAddress ip = InetAddress.getLocalHost();
+        String identifier = args[0];
+        String destination = args[1];
+        for (int i = 0; i < 1000; i++) {
+            //Sequence number is 0 on first packet sent 1 on second and it alternates as per the equation below.
+            int seq = i % 2;
+            //Formulate our message to be sent.
+            byte[] msg = (identifier+" "+destination+" "+seq).getBytes();
+            //Initialize new packet to be sent and send it.
+            DatagramPacket send = new DatagramPacket(msg, msg.length,ip,8888);
+            client.send(send);
+            //Sleep for half a second before sending next packet.
+            Thread.sleep(500);      
+        }
+
+    }
 }
