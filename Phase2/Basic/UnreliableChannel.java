e51e167e70c06e989b588be55e5d4d17c03c2991
alaa-sarrieddine <alaasarrieddine@gmail.com>
2024-11-20 18:35:15 +0200

    Implemented base functionality that allows clients to listen to requests from the server, and allows the server to map usernames to socket ports and forward messages to the correct destinations. three functions added with necessary comments: void recievePackets() (In UserClient.java) void serverSend() void addClient(DatagramPacket clientMessage) (Both in UnreliableChannel.java) TODO: implement packetloss, and delay simulation in serverSend() function.

--- Phase2/Basic/UnreliableChannel.java
+++ Phase2/Basic/UnreliableChannel.java
@@ -2,34 +2,113 @@ package Basic;
 
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Queue;
 
+//Each instance of the channel has its own socket,a map for client names and ports,a messagequeue,and the parameters.
 public class UnreliableChannel {
+    private static DatagramSocket server;
+    private static HashMap<String, Integer> clients;
+    private static Queue<DatagramPacket> messageQueue;
+    private static double prob;
+    private static int minD;
+    private static int maxD;
 
-    public static void main(String[] args)throws Exception {
-        //Make sure operator inputs correct amount of parameters.
-        if (args.length < 3) {
-            System.out.println("Please enter correct parameters.(java UnreliableChannel <p> <minD> <maxD>)");
+    // serverSend()
+    // Expects: Nothing
+    // Returns: nothing, but is responsible for going through the messagequeue and
+    // sending queued packets to their respective destinations.
+    public static void serverSend() throws Exception {
+        while (!messageQueue.isEmpty()) {
+            DatagramPacket i = messageQueue.poll();
+            // Store the message to be sent in a byte array.
+            byte[] message = i.getData();
+            // Get destination which is at index 2 our string and of length 1 ("A B 0")
+            String destination = new String(i.getData(), 2, 1);
+            // Get the port of the destination from the hashmap.
+            int port;
+            if (clients.containsKey(destination)) {
+                port = clients.get(destination);
+            } else {
+                System.out.println("Invalid client packet not sent");
+                return;
+            }
+            // Intialize new packet to be sent. Note that since this is running locally we
+            // do not need to get the IP address of the destination,only the port.
+            DatagramPacket send = new DatagramPacket(message, message.length, InetAddress.getLocalHost(), port);
+            try {
+                server.send(send);
+            } catch (Exception e) {
+                System.out.println("failed to send");
+                return;
+            }
+
         }
-        double p = Double.parseDouble(args[0]);
-        int minD = Integer.parseInt(args[1]);
-        int maxD = Integer.parseInt(args[2]);
-        int testing = 0;
-        //Create socket at port 8888 to listen for client messages.
-        DatagramSocket server = new DatagramSocket(8888);
+
+    }
+
+    // addclient(DatagramPacket cleintMessage):
+    // Expects: A DatagraPacket
+    // Returns: nothing, but maps the sourcename to the local port.
+    public static void addClient(DatagramPacket clientMessage) {
+        // Get sourceName which is at index 0 our string and of length 1 ("A B 0")
+        String sourceName = new String(clientMessage.getData(), 0, 1);
+        if (!clients.containsKey(sourceName)) {
+            clients.put(sourceName, clientMessage.getPort());
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        // Make sure operator inputs correct amount of parameters.
+        if (args.length < 3) {
+            System.out.println("Please enter correct parameters.(java UnreliableChannel <prob> <minD> <maxD>)");
+        }
+        prob = Double.parseDouble(args[0]);
+        minD = Integer.parseInt(args[1]);
+        maxD = Integer.parseInt(args[2]);
+        clients = new HashMap<>();
+        messageQueue = new LinkedList<>();
+        int nOfMessages = 0;
+        boolean both = false;
+
+        // Create socket at port 8888 to listen for client messages, and facilitate the
+        // sending of messages.
+        server = new DatagramSocket(8888);
         byte[] buffer = new byte[1024];
         while (true) {
-            //Initialize packet to be recieved and store data in the buffer. Then print it out.
+            nOfMessages++;
+            // Initialize packet to be recieved and store data in the buffer.
             DatagramPacket messageFromClient = new DatagramPacket(buffer, buffer.length);
             server.receive(messageFromClient);
-            //Here we initialize a string from the data we get from the client, however we make sure to start reading from 0 only untill the length of the message.
-            //This prevents the slots that arent written to affecting the output
-            String message = new String(messageFromClient.getData(),0,messageFromClient.getLength());
-            System.err.println(message);
-            //When client sends end message break.
+            // Store our client in a hashmap to easily forward messages.
+            if (clients.size() < 2) {
+                addClient(messageFromClient);
+            }
+            // Here we initialize a string from the data we get from the client, however we
+            // make sure to start reading from 0 only untill the length of the message.
+            // This prevents the slots that arent written too affecting the output.
+            String message = new String(messageFromClient.getData(), 0, messageFromClient.getLength());
+            System.out.println(message);
+            // When client sends end message, check if both clients have ended then stop.
+            //Note that in the cases where we recieve END signals we do not add them to the message Queue as they are not valid packets to be sent.
             if (message.equals("END")) {
-                break;
+                nOfMessages--;
+                if (both) {
+                    break;
+                }
+                both = true;
+                continue;
+            }
+            // Add message to queue, and if we have the needed number of clients then we
+            // initiate sending from the server.
+            messageQueue.add(messageFromClient);
+            if (clients.size() >= 2) {
+                serverSend();
             }
         }
-        System.out.println("Client sent end message!");
+        System.out.println("Clients sent end messages! Messages recieved: " + nOfMessages);
+        server.close();
     }
 }
--- Phase2/Basic/UserClient.java
+++ Phase2/Basic/UserClient.java
@@ -3,17 +3,42 @@ package Basic;
 import java.net.*;
 
 public class UserClient {
+    private static DatagramSocket client;
+
+    // recievePackets:
+    // Expects:Nothing
+    // Returns:Nothing, but listens for a packet for a period of 0.5 seconds, if
+    // a packet is recieved its contents are printed, otherwise it times out and no
+    // packets are recieved
+    public static void recievePackets() throws Exception {
+        byte[] buffer = new byte[1024];
+        DatagramPacket recieved = new DatagramPacket(buffer, buffer.length);
+        try {
+            client.receive(recieved);
+        } catch (Exception e) {
+            System.out.println("Timed out no packet recieved");
+            return;
+        }
+        String message = new String(recieved.getData(), 0, recieved.getLength()).trim();
+        System.out.println("Message recieved:" + message);
+    }
+
     public static void main(String[] args) throws Exception {
-        // We make sure sure inputs the source name and the destination.
+        // We make sure the operator inputs the source name and the destination.
         if (args.length < 2) {
             System.out.println("Please enter source, and destination(java Userclient <source> <destination>)");
             return;
         }
-        DatagramSocket client = new DatagramSocket();
+        // Initalize socket for client to send and recieve packets, then set timeout to
+        // 0.5 seconds.
+        client = new DatagramSocket();
+        client.setSoTimeout(500);
         InetAddress ip = InetAddress.getLocalHost();
         String identifier = args[0];
         String destination = args[1];
-        for (int i = 0; i < 1000; i++) {
+        // note we are only sending 10 packets for testing purposes MUST BE CHANGED FOR
+        // FINAL VERSION!!!
+        for (int i = 0; i < 10; i++) {
             // Sequence number is 0 on first packet sent 1 on second and it alternates as
             // per the equation below.
             int seq = i % 2;
@@ -22,13 +47,17 @@ public class UserClient {
             // Initialize new packet to be sent and send it.
             DatagramPacket send = new DatagramPacket(msg, msg.length, ip, 8888);
             client.send(send);
+            // Listen for incoming packets before sleeping.
+            recievePackets();
             // Sleep for half a second before sending next packet.
             Thread.sleep(500);
+
         }
         // Send final packet with end message.
         byte[] endMessage = "END".getBytes();
         DatagramPacket last = new DatagramPacket(endMessage, endMessage.length, ip, 8888);
         client.send(last);
         System.out.println("Client: " + identifier + " done sending.");
+        client.close();
     }
 }
